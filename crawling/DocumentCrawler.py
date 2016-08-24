47d788f2d108faa56eae6ff67c5ca4f2cb9a1ce4
gioGats <ryangiarusso@gmail.com>
2016-08-24 15:48:12 -0500

    Doc Crawler Troubleshooting

--- crawling/DocumentCrawler.py
+++ crawling/DocumentCrawler.py
@@ -16,7 +16,7 @@ class DocumentCrawler(object):
 
         # Handle mutable defaults
         if forms_to_download is None:
-            forms_to_download = ['10-K', '10-Q']
+            forms_to_download = ['10-K', '10-Q', '10-K/A', '10-Q/A']
         if timeout is not None:
             timeout = datetime.timedelta(hours=timeout)
         start = datetime.datetime.now()
@@ -25,10 +25,7 @@ class DocumentCrawler(object):
         ftp = EdgarFtp()
 
         # Init cik_list
-        #cik_list = pickle.load(open('EDGAR/objects/ref/CIK_List.pkl', 'rb'))
-        # TODO Change back to full cik_list
-        cik_list = ['0000320193', '1652044']
-
+        cik_list = pickle.load(open('EDGAR/objects/ref/CIK_List.pkl', 'rb'))
 
         # Update directory structure
         update_file_structure()
@@ -46,7 +43,6 @@ class DocumentCrawler(object):
         success = 0
         fail = 0
         previously_complete = 0
-        dont_exist = 0
         exit_code = None
 
         # Know the current quarter
@@ -62,49 +58,54 @@ class DocumentCrawler(object):
 
         # Crawler Loop
         try:
-            # Loop backwards in time
-            # Prioritize downloading more recent documents
+            # Loop backwards in time to prioritize more recent documents
             year = end_year
             while year >= start_year:
                 for qtr in [4, 3, 2, 1]:
+                    print('%sQTR%s' % (str(year), str(qtr)))
 
                     # Ignore quarters that haven't finished yet
-                    # FUTURE Add current quarter updating
                     if year == current_year and qtr >= current_qtr:
+                        # FUTURE Add current quarter updating
                         continue
 
+                    # Search both index files
                     for index in ['master.idx', 'xbrl.idx']:
                         # Open and load the index file
                         directory = 'full-index/' + str(year) + '/QTR' + str(qtr) + '/'
                         index_file = open(directory + index, 'r')
 
-                        for cik in cik_list:
-                            for form in forms_to_download:
-                                local_addr = self.local_form_address(cik, form, year, qtr)
-                                # TODO print('\rCurrent File: %s' % local_addr, end='')
-                                if not os.path.exists(local_addr):
-                                    print('CIK %s; Form %s' % (cik, form))
-                                    edgar_addr = self.find_form_address(cik, form, index_file)
-                                    if edgar_addr is None:
-                                        dont_exist += 1
-                                        error_log += str(datetime.datetime.now()) \
-                                                     + ': Failed to locate form %s: %sQTR%s CIK %s\n' \
-                                                       % (form, str(year), str(qtr), cik)
-                                    else:
+                        header = True
+                        for line in index_file:
+                            line = line.replace('\n', '')
+                            if header:
+                                # Filter out the header
+                                if '------------' in line:
+                                    header = False
+                            else:
+                                line_list = line.split('|')
+                                cik = str(int(line_list[0]))
+                                form = line_list[2]
+                                if cik in cik_list and form in forms_to_download:
+                                    print('\rFound %s for %s' % (form, cik), end='')
+                                    total += 1
+                                    edgar_addr = line_list[4]
+                                    local_addr = self.local_form_address(cik, form, year, qtr)
+                                    if not os.path.exists(local_addr):
                                         try:
                                             ftp.download(edgar_addr, local_addr)
                                             success += 1
                                         except Exception:
                                             # Log errors
                                             error_log += str(datetime.datetime.now()) \
-                                                         + ': Failed to download ' + local_addr + '\n'
+                                                         + ': Failed to download ' + edgar_addr + '\n'
                                             fail += 1
-                                else:
-                                    previously_complete += 1
-                                if timeout is not None:
-                                    if datetime.datetime.now() - start > timeout:
-                                        exit_code = 'Timeout'
-                                        sys.exit()
+                                    else:
+                                        previously_complete += 1
+                            if timeout is not None:
+                                if datetime.datetime.now() - start > timeout:
+                                    exit_code = 'Timeout'
+                                    sys.exit()
                 year -= 1
             exit_code = 'Loop complete'
             sys.exit()
@@ -116,9 +117,9 @@ class DocumentCrawler(object):
             if exit_code is None:
                 exit_code = 'Unknown'
             this_log += 'Exit: %s\nPreviously Complete: %d\nSuccessful: %d\nFailed: %d\n' \
-                        'Unattempted: %d\nNot Found: %d\n' % \
+                        'Unattempted: %d\n' % \
                         (exit_code, previously_complete, success,
-                         fail, total - (previously_complete + success + fail), dont_exist)
+                         fail, total - (previously_complete + success + fail))
             log_file.write(this_log + '\n#####################\n#####################\n')
             log_file.write('\n' + error_log)
             log_file.close()
