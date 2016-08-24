872c793532478b94df4b217b742a27b70e70be83
gioGats <ryangiarusso@gmail.com>
2016-08-23 22:01:20 -0500

    Initial doc crawler work

--- CLI.py
+++ CLI.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import sys
 from crawling.IndexCrawler import IndexCrawler
+from crawling.DocumentCrawler import DocumentCrawler
 
 # CLI for EDGAR project
 # Current implementations:
-# -test_ftp
+# -automated
 # -index_crawler
+# -document_crawler
 
 fn = sys.argv[1]
 arg1 = sys.argv[2]
@@ -13,11 +15,22 @@ arg2 = sys.argv[3]
 arg3 = sys.argv[4]
 arg4 = sys.argv[5]
 
-if fn == '-t':
-    print(str(sys.argv))
+if fn == '-a' or fn == 'auto':
+    settings = {}
+    with open('crawling/crawling_settings.txt', 'r') as f:
+        for line in f:
+            if "EDGAR" in line:
+                continue
+            else:
+                line = line.replace(' ', '').split(':')
+                settings[line[0]] = line[1]
+    settings['forms'] = settings['forms'].split(',')
 
-elif fn == 'test_ftp':
-    pass
+    ic = IndexCrawler()
+    ic.crawl(settings['start_year'], settings['end_year'], None, settings['index_timeout'])
+
+    dc = DocumentCrawler()
+    dc.crawl(settings['start_year'], settings['end_year'], settings['forms'], settings['doc_timeout'])
 
 elif fn == '-i' or fn == 'index_crawler':
     # Validate input
@@ -46,7 +59,29 @@ elif fn == '-i' or fn == 'index_crawler':
     ic.crawl(start, end, to_download, timeout)
 
 elif fn == '-d' or fn == 'document_crawler':
-    raise NotImplemented
+    # Validate input
+    try:
+        start = int(arg1)
+    except ValueError:
+        start = 1993
+    try:
+        end = int(arg2)
+    except ValueError:
+        end = 2016
+    try:
+        if arg3 == '-d':
+            to_download = None
+        else:
+            to_download = arg3.replace('[', '').replace(']', '').replace("'", '').replace(' ', '').split(',')
+    except ValueError:
+        to_download = None
+    try:
+        timeout = float(arg4)
+    except ValueError:
+        timeout = None
+
+    dc = DocumentCrawler()
+    dc.crawl(start, end, to_download, timeout)
 
 elif fn == '-c' or fn == 'update_cik':
     raise NotImplemented
---
+++ crawling/DocumentCrawler.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+from objects.FTP import EdgarFtp
+import os
+import datetime
+import pickle
+import sys
+
+
+class DocumentCrawler(object):
+    def __init__(self):
+        pass
+
+    def crawl(self, start_year=2000, end_year=2016, forms_to_download=None, timeout=None):
+
+        # Handle mutable defaults
+        if forms_to_download is None:
+            forms_to_download = ['10-K', '10-Q']
+        if timeout is not None:
+            timeout = datetime.timedelta(hours=timeout)
+        start = datetime.datetime.now()
+
+        # Init ftp object
+        ftp = EdgarFtp()
+
+        # Init cik_list
+        cik_list = pickle.load('EDGAR/objects/ref/CIK_List.pkl')
+
+        # Open new log file
+        log_file = open('EDGAR/crawling/log_DocCrawler_%s.txt' % (str(datetime.datetime.now())), 'w')
+        this_log = '####### Doc Crawler Summary #######\nRun' + str(datetime.datetime.now()) + '\n'
+        error_log = '####### Doc Crawler Errors #######\nRun' + str(datetime.datetime.now()) + '\n'
+
+        # Change to the large storage directory
+        os.chdir('/storage/')
+
+        # Init counters
+        total = 0
+        success = 0
+        fail = 0
+        previously_complete = 0
+        dont_exist = 0
+        exit_code = None
+
+        # Know the current quarter
+        current_year = datetime.date.today().year
+        if 1 <= datetime.date.today().month <= 3:
+            current_qtr = 1
+        elif 4 <= datetime.date.today().month <= 6:
+            current_qtr = 2
+        elif 7 <= datetime.date.today().month <= 9:
+            current_qtr = 3
+        else:
+            current_qtr = 4
+
+        # Crawler Loop
+        try:
+            # Loop backwards in time
+            # Prioritize downloading more recent documents
+            year = end_year
+            while year >= start_year:
+                for qtr in [4, 3, 2, 1]:
+
+                    # Ignore quarters that haven't finished yet
+                    # FUTURE Add current quarter updating
+                    if year == current_year and qtr >= current_qtr:
+                        continue
+
+                    for index in ['master.idx, xbrl.idx']:
+                        # Open and load the index file
+                        directory = 'full-index/' + str(year) + '/QTR' + str(qtr) + '/'
+                        index_file = open(directory + index, 'r')
+
+
+                        for cik in cik_list:
+                            for form in forms_to_download:
+                                if not os.path.exists(self.local_form_address(cik, form, year, qtr)):
+                                    edgar_addr = self.find_form_address(cik, form, index_file)
+                                    if edgar_addr is None:
+                                        dont_exist += 1
+                                        error_log += str(datetime.datetime.now()) \
+                                                         + ': Failed to locate form ' + edgar_addr + '\n'
+                                    else:
+                                        try:
+                                            ftp.download(edgar_addr)
+                                            success += 1
+                                        except Exception:
+                                            # Log errors
+                                            error_log += str(datetime.datetime.now()) \
+                                                         + ': Failed to download ' + edgar_addr + '\n'
+                                            fail += 1
+                                else:
+                                    previously_complete += 1
+                                if timeout is not None:
+                                    if datetime.datetime.now() - start > timeout:
+                                        exit_code = 'Timeout'
+                                        sys.exit()
+                year -= 1
+            exit_code = 'Loop complete'
+            sys.exit()
+
+        except (KeyboardInterrupt, SystemExit) as e:
+            print('\n')
+            if isinstance(e, KeyboardInterrupt):
+                exit_code = 'Keyboard interrupt'
+            if exit_code is None:
+                exit_code = 'Unknown'
+            this_log += 'Exit: %s\nPreviously Complete: %d\nSuccessful: %d\nFailed: %d\n' \
+                        'Unattempted: %d\nNot Found: %d\n' % \
+                        (exit_code, previously_complete, success,
+                         fail, total-(previously_complete+success+fail), dont_exist)
+            log_file.write(this_log + '\n#####################\n#####################\n')
+            log_file.write('\n' + error_log)
+            log_file.close()
+
+
+    def find_form_address(self, cik, form, index_file):
+        header = True
+        for line in index_file:
+            if header:
+                if '------------' in line:
+                    header = False
+            else:
+                line_list = line.split('|')
+                if cik == line_list[0] and form == line_list[2]:
+                    return line_list[4]
+        return None
+
+
+    def local_form_address(self, cik, form, year, qtr):
+        return '/storage/cik/%s_%sQ%s_%s.txt' % (str(cik), str(year), str(qtr), str(form))
