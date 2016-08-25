565b2ba24163389c253a72dd4d6eed66e8c9a937
gioGats <ryangiarusso@gmail.com>
2016-08-25 12:09:07 -0500

    Create setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='EDGAR',
+      version='0.1',
+      description='EDGAR Collection Module',
+      long_description='A collection of tools for gathering and interpreting '
+                       'financial filings from the SEC EDGAR database.',
+      author='Ryan Giarusso',
+      author_email='ryangiarusso@gmail.com',
+      url='https://github.com/gioGats/EDGAR',
+      download_url='https://github.com/gioGats/EDGAR/archive/master.zip',
+      packages=['crawling', 'objects', 'objects.ref'],
+      platforms=['UNIX'],
+      license='Apache License Version 2.0'
+      )
