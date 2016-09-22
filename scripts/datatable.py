1cb57c19d37934b15e46074d44e1d19ba3c3bcfc
gioGats <ryangiarusso@gmail.com>
2016-09-21 23:35:42 -0500

    Added dtg

--- scripts/datatable.py
+++ scripts/datatable.py
@@ -24,18 +24,18 @@ def add_data(doc, path):
 
 def add_acceptance_dtg(doc, path):
     d = open(path + '/' + doc, 'r')
-    line = d.read().splitlines()[3]
+    line = d.read().splitlines()[2]
     d.close()
     acceptance_datetime = line.split('>')[1]
-    year = acceptance_datetime[:4]
-    month = acceptance_datetime[4:6]
-    day = acceptance_datetime[6:8]
-    hour = acceptance_datetime[8:10]
-    minute = acceptance_datetime[10:12]
-    second = acceptance_datetime[12:14]
+    year = int(acceptance_datetime[:4])
+    month = int(acceptance_datetime[4:6])
+    day = int(acceptance_datetime[6:8])
+    hour = int(acceptance_datetime[8:10])
+    minute = int(acceptance_datetime[10:12])
+    second = int(acceptance_datetime[12:14])
     dtg = datetime(
         year=year, month=month, day=day, hour=hour, minute=minute, second=second,
-        tzinfo=pytz.pytz.timezone('US/Eastern'))
+        tzinfo=pytz.timezone('US/Eastern'))
     return str(dtg)
 
 
